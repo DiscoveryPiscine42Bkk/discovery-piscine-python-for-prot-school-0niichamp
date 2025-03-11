e55410d32807a4deadd05614e8a42145e90c288e
0niichamp <163907782+0niichamp@users.noreply.github.com>
2025-03-11 11:55:59 +0700

    Add files via upload

---
+++ cell03/ex32/i_got_that.py
@@ -0,0 +1,10 @@
+say = str(input("What you gonna say? : "))
+els = str(input("I got that! Anything else? :"))
+end = "STOP"
+
+while 1 :
+    els = str(input("I got that! Anything else? :"))
+
+    if els == end:
+        break
+    
